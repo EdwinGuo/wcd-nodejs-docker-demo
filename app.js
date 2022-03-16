2aea8c400d093703d57e48396bcc86d746ee2e58
eguo <altergzj@yahoo.com>
2022-03-16 19:23:08 -0400

    Make a tiny change on the js code

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  console.log('helloworld')
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
